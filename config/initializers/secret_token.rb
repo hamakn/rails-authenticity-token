3d6483a2ecc9222c0f401d333bd9a7d81b7a513e
Katsunori Kawaguchi <hamakata@gmail.com>
2012-07-30 19:10:33 +0900

    rails _3.1.7_ new apitest -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apitest::Application.config.secret_token = 'd33a1c3d97251d1ef3b696db2b779676b093c5d24387aa6cd68fcb4100c115bf7ef3e87b2dd03dc44c8497175f725c61a466d37eb14838b0c9ad50fd4c35db35'
